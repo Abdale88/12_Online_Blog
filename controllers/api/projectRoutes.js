94773f872700578584459fa731fc3d57a379b31c
Abdale Hassan <cabdale989@gmail.com>
2021-06-21 15:34:10 -0500

    fixing error

---
+++ controllers/api/projectRoutes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router();
+const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+
+      // username: req.body.username,
+      // email: req.body.email,
+      // password: req.body.password,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const projectData = await Project.update(
+      {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+      {
+        where:{ 
+          id: req.params.id,
+        }
+      }
+    );
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ public/js/updatedBlog.js
@@ -0,0 +1,26 @@
+const updateFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#title').value.trim();
+    const description = document.querySelector('#title-desc').value.trim();
+    
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+  
+      const response = await fetch(`/api/projects${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create project');
+      }
+  };
+  
+  document.querySelector('.update-project-form').addEventListener('submit', updateFormHandler);
