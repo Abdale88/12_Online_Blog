d90dce2036c7d145f73eb044798e65f31cbf4416
Abdale Hassan <cabdale989@gmail.com>
2021-06-23 18:53:18 -0500

    add gitignore

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,5 +1,4 @@
 const logout = async () => {
-<<<<<<< HEAD
   const response = await fetch('/api/users/logout', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -13,19 +12,3 @@ const logout = async () => {
 };
 
 document.querySelector('#logout').addEventListener('click', logout);
-=======
-    const response = await fetch('/api/users/logout', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-  document.querySelector('#logout').addEventListener('click', logout);
-  
->>>>>>> ecd1c6b778341a7651dd06dd99ce820192982cc0
