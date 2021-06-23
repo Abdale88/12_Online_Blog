347662ea96efa13d1fd5d5168383a582d5fdb30a
Abdale Hassan <cabdale989@gmail.com>
2021-06-23 17:24:34 -0500

    add js file to delete ar a particular blog

---
+++ public/js/delete.js
@@ -0,0 +1,27 @@
+const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      if(id){   
+        const response = await fetch(`/api/blogs/${id}`, {
+          method: 'DELETE',
+        });
+    
+        if (response.ok) {
+          document.location.replace('/dashboard');
+        } else {
+          alert('Failed to delete blog');
+        }
+      }
+      }
+  };
+  
+  
+  const deleteButton = document.querySelectorAll('.delete-blog');
+  
+  if (document.querySelector('.delete-blog')) {
+      deleteButton.forEach((b) => {
+          b.addEventListener('click', delButtonHandler);
+      })
+  
+  }  
